ac223e2b848a68fc9153e0eeb6bcb239abd028f0
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2024-11-12 16:59:14 +0200

    designing picture album website

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,32 @@
-document.getElementById('currentyear').innerHTML = new Date().getFullYear()
-document.getElementById('lastmodified').innerHTML = document.lastModified
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastmodified").innerHTML = document.lastModified;
+
+let bar = document.getElementById("bars");
+let display = true;
+const menu = document.getElementById("menu");
+const crossbars = document.getElementById("crossbars");
+
+function Display() {
+  // check if a menu is displaying
+  if (display) {
+    // display or hide the menu
+    menu.style.display = "flex";
+    crossbars.style.display = "flex";
+    bar.style.display = "none";
+    display = false;
+  }
+}
+function Hide() {
+    // check if a menu is displaying
+    if (display === false) {
+      // display or hide the menu
+      menu.style.display = "none";
+      crossbars.style.display = "none";
+      bar.style.display = "block";
+      display = true;
+    }
+  }
+  
+
+document.getElementById("bars").addEventListener("click", Display);
+document.getElementById("crossBars").addEventListener("click", Hide);
